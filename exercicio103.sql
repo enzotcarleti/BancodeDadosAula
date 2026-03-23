41b401a6f16d049bfd4082e835673fda4ffc710e
enzotcarleti <enzo.ct@puccampinas.edu.br>
2026-03-23 09:30:06 -0300

    Update SQL script with table and data changes
    Added various SQL commands for table modifications and data insertion.

--- exercicio103.sql
+++ exercicio103.sql
@@ -1,20 +1,44 @@
 USE BD12022677;
 SHOW TABLES;
-CREATE TABLE funcionarios (id INT PRIMARY KEY, nome VARCHAR(100) NOT NULL, cargo VARCHAR(50) NOT NULL, salario DECIMAL(12,2));
-SELECT * FROM funcionarios;
 
-ALTER TABLE funcionarios
-MODIFY COLUMN departamento_id INT;
+Exercicios Banco de Dados - Enzo Carleti Teixeira
 
-CREATE TABLE departamentos (id INT PRIMARY KEY, nome VARCHAR(100) NOT NULL);
+1 - Não tem como na puc
+
+2 - CREATE TABLE funcionarios (id INT PRIMARY KEY, nome VARCHAR(100) NOT NULL, cargo VARCHAR(50) NOT NULL, salario DECIMAL(12,2));
+
+INSERT INTO funcionarios (id, nome, cargo, salario) VALUES
+("Ana", "Analista", "10020")
+("Bruno", "Dev Jr", "2000");
+
+3 - ALTER TABLE funcionarios
+ADD COLUMN data_admissao DATE NOT NULL;
+
+update funcionarios set data_admissao "2026-01-01";
+
+4 - ALTER TABLE funcionarios
+DROP COLUMN cargo;
+
+5 - ALTER TABLE funcionarios
+MODIFY COLUMN salario DECIMAL(12,2);
+
+6 - CREATE TABLE departamentos (id INT PRIMARY KEY, nome VARCHAR(100) NOT NULL);
 ALTER TABLE funcionarios
 ADD FOREIGN KEY (departamento_id) REFERENCES departamentos (id);
 
+7 - ALTER TABLE departamentos
+CREATE COLUMN orcamento DECIMAL(12,2);
 
+8 - ALTER TABLE departamentos
+DROP COLUMN orcamento;
 
-
-ALTER TABLE departamentos
+9 - ALTER TABLE departamentos
 MODIFY COLUMN nome VARCHAR(150);
 
-ALTER TABLE departamentos
+10 - ALTER TABLE departamentos
 RENAME TO setores;
+
+11 - DROP TABLE setores;
+
+12 - ALTER TABLE funcionarios
+ADD COLUMN status VARCHAR(20) DEFAULT "Ativo";
